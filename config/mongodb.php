b2bf684ae73b429ee3dd13158905380b8290e958
dimaoleynikov <oleynikov.dima@gmail.com>
2026-05-13 21:07:35 +0300

    Add MongoDB support and users table
    - Introduced MongoDB integration via `yiisoft/yii2-mongodb` package.
    - Updated `docker-compose.yml` to include `mongodb` and `db` services with appropriate configurations.
    - Added `mongodb.php` configuration file and registered it in `web.php` and `console.php`.
    - Modified `db.php` to use the `db` service credentials.
    - Created migration for `users` table with relevant schema.

---
+++ config/mongodb.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'class' => 'yii\mongodb\Connection',
+    'dsn' => 'mongodb://mongodb:27017/yii2basic',
+];
--- config/web.php
+++ config/web.php
@@ -2,6 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
+$mongodb = require __DIR__ . '/mongodb.php';
 
 $config = [
     'id' => 'basic',
@@ -47,6 +48,7 @@ $config = [
             ],
         ],
         'db' => $db,
+        'mongodb' => $mongodb,
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
